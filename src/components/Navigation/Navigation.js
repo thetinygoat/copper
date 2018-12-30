a5a239e7b678e86349f4aa86a66b8307dc13de31
Sachin Saini <iamsainisachin@gmail.com>
2018-12-30 20:42:37 +0530

    finish navigation bar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,41 @@
 import React from "react";
+import {
+	Navbar,
+	NavbarGroup,
+	NavbarHeading,
+	Colors,
+	Alignment,
+	Button,
+	Intent,
+	Popover,
+	Position
+} from "@blueprintjs/core";
+
+import DropDown from "./DropDown/DropDown";
 
 export default function Navigation() {
 	return (
 		<div>
-			<p>Navigation Bar</p>
+			<Navbar style={{ background: Colors.DARK_GRAY2, color: Colors.WHITE }}>
+				<Navbar.Group align={Alignment.LEFT}>
+					<NavbarHeading>COPPER</NavbarHeading>
+				</Navbar.Group>
+				<Navbar.Group align={Alignment.RIGHT}>
+					<Popover content={<DropDown type="add" />} position={Position.BOTTOM}>
+						<Button icon="plus" minimal={true} />
+					</Popover>
+					<Button minimal={true} style={{ color: Colors.WHITE }} icon="log-in">
+						Login
+					</Button>
+					<Button
+						text="Logout"
+						icon="log-out"
+						minimal={true}
+						style={{ color: Colors.WHITE }}
+					/>
+					<Button intent={Intent.SUCCESS} text="Sign up" />
+				</Navbar.Group>
+			</Navbar>
 		</div>
 	);
 }
