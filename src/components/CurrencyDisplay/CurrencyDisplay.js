7d3db538d578749f3d7a14f62be44c933fe2c71b
Sachin Saini <iamsainisachin@gmail.com>
2018-12-31 00:55:50 +0530

    add currency display component

---
+++ src/components/CurrencyDisplay/CurrencyDisplay.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Divider } from "@blueprintjs/core";
+
+export default function CurrencyDisplay(props) {
+	return (
+		<div>
+			<p style={{ fontSize: "1.3em" }}>{props.type}</p>
+			<Divider vertical={true} />
+			<h2 className="bp3-heading">
+				<span
+					style={{
+						fontSize: ".7em",
+						position: "relative",
+						top: "-10px",
+						fontWeight: "normal"
+					}}
+				>
+					{props.currency}
+				</span>
+				{props.value}
+			</h2>
+		</div>
+	);
+}
