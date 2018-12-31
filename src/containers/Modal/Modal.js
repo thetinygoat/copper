cd1e9528b43a56e3e7fadf85ef0b5dd7a152e698
Sachin Saini <iamsainisachin@gmail.com>
2018-12-31 23:52:31 +0530

    add form to modal directly instead of passing as children

--- src/containers/Modal/Modal.js
+++ src/containers/Modal/Modal.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Dialog, Classes, Button } from "@blueprintjs/core";
 import { connect } from "react-redux";
 import * as modalActions from "../../store/actions/modal";
+import Form from "../../containers/Form/Form";
 
 class Modal extends Component {
 	render() {
@@ -9,11 +10,10 @@ class Modal extends Component {
 			<Dialog
 				icon={this.props.icon}
 				onClose={this.props.handleClose}
-				title={this.props.title}
 				{...this.props.modal}
 			>
 				<div className={Classes.DIALOG_BODY}>
-					{this.props.children}
+					<Form type={this.props.modal.formType} />
 					<div className={Classes.DIALOG_FOOTER}>
 						<div className={Classes.DIALOG_FOOTER_ACTIONS}>
 							<Button>Submit</Button>
